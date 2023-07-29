4c012f68ed2f15721a7fc3140daf37a9470d2bcf
Alok Sharma <2021eo262al@mitsgwl.ac.in>
2023-07-29 20:36:41 +0530

    Create index.js

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import iot from "./iot.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import github_logo from "./tech/github_logo.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import bharat_intern from "./company/bharat_intern.png";
+import tesla from "./company/tesla.png";
+
+import quizzify from "./quizzify.png";
+import resume from "./resume.png";
+import codevault from "./codevault.png";
+
+export {
+  logo,
+  backend,
+  iot,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  github_logo,
+  figma,
+  git,
+  python,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  bharat_intern,
+  tesla,
+  quizzify,
+  resume,
+  codevault,
+};
